c968822a05805b11e110535427f02289fcd81f26
Moritz <mgsoda0@gmail.com>
2026-08-02 15:39:00 +0200

    Add missing import for KinoKing in index.ts

--- src/source/index.ts
+++ src/source/index.ts
@@ -2,12 +2,13 @@ import { envGet, Fetcher } from '../utils';
 import { Einschalten } from './Einschalten';
 import { FilmpalastTO } from './FilmpalastTO';
 import { KinoGer } from './KinoGer';
+import { KinoKing } from './KinoKing'; // <-- Hier ist der fehlende Import!
 import { MegaKino } from './MegaKino';
 import { MeineCloud } from './MeineCloud';
 import { Moflix } from './moflix';
 import { Source } from './Source';
-export * from './KinoKing';
 
+export * from './KinoKing';
 export * from './Source';
 
 export const createSources = (fetcher: Fetcher): Source[] => {
