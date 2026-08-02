97cf05d633e60513f8b872288b56ee2ddec59c4c
Moritz <mgsoda0@gmail.com>
2026-08-02 14:02:35 +0200

    Remove unused source imports and update sources

--- src/source/index.ts
+++ src/source/index.ts
@@ -1,27 +1,11 @@
 import { envGet, Fetcher } from '../utils';
-import { CineHDPlus } from './CineHDPlus';
-import { Cuevana } from './Cuevana';
 import { Einschalten } from './Einschalten';
-import { Eurostreaming } from './Eurostreaming';
 import { FilmpalastTO } from './FilmpalastTO';
-import { FourKHDHub } from './FourKHDHub';
-import { Frembed } from './Frembed';
-import { FrenchCloud } from './FrenchCloud';
-import { HDHub4u } from './HDHub4u';
-import { HomeCine } from './HomeCine';
 import { KinoGer } from './KinoGer';
-import { Kokoshka } from './Kokoshka';
 import { MegaKino } from './MegaKino';
 import { MeineCloud } from './MeineCloud';
-import { MostraGuarda } from './MostraGuarda';
-import { MovieBox } from './MovieBox';
-import { Movix } from './Movix';
-import { Source } from './Source';
-import { VerHdLink } from './VerHdLink';
-import { VidSrc } from './VidSrc';
-import { Vidzee } from './Vidzee';
-import { VixSrc } from './VixSrc';
 import { Moflix } from './moflix';
+import { Source } from './Source';
 
 export * from './Source';
 
@@ -29,33 +13,12 @@ export const createSources = (fetcher: Fetcher): Source[] => {
   const disabledSources = envGet('DISABLED_SOURCES')?.split(',') ?? [];
 
   return [
-    // multi
-    new FourKHDHub(fetcher),
-    new HDHub4u(fetcher),
-    new VixSrc(fetcher),
-    new VidSrc(),
-    new Vidzee(fetcher),
-    new MovieBox(fetcher),
-    // AL
-    new Kokoshka(fetcher),
-    // ES / MX
-    new CineHDPlus(fetcher),
-    new Cuevana(fetcher),
-    new HomeCine(fetcher),
-    new VerHdLink(fetcher),
-    // DE
+    // DE (Nur deutsche Quellen)
     new Einschalten(fetcher),
     new KinoGer(fetcher),
     new MegaKino(fetcher),
     new MeineCloud(fetcher),
     new FilmpalastTO(fetcher),
     new Moflix(fetcher),
-    // FR
-    new Frembed(fetcher),
-    new FrenchCloud(fetcher),
-    new Movix(fetcher),
-    // IT
-    new Eurostreaming(fetcher),
-    new MostraGuarda(fetcher),
   ].filter(source => !disabledSources.includes(source.id));
 };
